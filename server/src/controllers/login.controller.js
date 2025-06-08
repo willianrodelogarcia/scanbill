2bfdc79c1af84aa33b8fb7ee6e1ca09eae9ec03a
Willian Rodelo <wrodelogarcia@gmail.com>
2025-06-07 23:29:44 -0500

    change to none

--- server/src/controllers/login.controller.js
+++ server/src/controllers/login.controller.js
@@ -26,7 +26,7 @@ const login = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     path: '/',
-    secure: false, // Set to true if using HTTPS
+    secure: true, // Set to true if using HTTPS
     sameSite: 'none', // Adjust based on your requirements
   });
 
